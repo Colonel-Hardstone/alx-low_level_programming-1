4a64a2273bdf21b1cf9b86d0e5b3a9661c2df87f
Maereg Sahilu <85615737+MaeregSahilu@users.noreply.github.com>
2022-10-13 16:42:43 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,6 +3,6 @@
 /* this is the main function */
 int main(void)
 {
-	puts ("Programming is like building a multilingual puzzle\n");
-	return (0);
+	puts("Programming is like building a multilingual puzzle\n");
+	return(0);
 }
