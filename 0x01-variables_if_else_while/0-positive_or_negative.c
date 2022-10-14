7287dfb2f45b1642f0c42200d602fd0546ea863a
MaeregSahilu <maereg.sahilu.a@gmail.com>
2022-10-14 09:47:40 +0000

    Nesting is hard

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -19,15 +19,11 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	if (n > 0)
-	{
 		printf("is positive");
-	}
-	else if (n == 0)
-	{
-		printf("is zero");
-	}
 	else
-	{
+		if (n == 0) 
+		printf("is zero");
+		else
 		printf("is negative");
 	return (0);
 }
