2e5a618d4f75f4b6637d66d49d0b0b9134d5c55b
MaeregSahilu <maereg.sahilu.a@gmail.com>
2022-10-18 13:15:57 +0000

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+   *main - This function prints _putchar using putchar
+   *
+   *Return: This function returns 0 on success
+   */
+
+int main(void)
+{
+	char print[9];
+
+	print [0] = 95;
+	print [1] = 112;
+	print [2] = 117;
+	print [3] = 116;
+	print [4] = 99;
+	print [5] = 104;
+	print [6] = 97;
+	print [7] = 114;
+	print [8] = 10;
+
+	for (int i = 0; i <= 8; ++i)
+		putchar(print[i]);
+
+	return (0);
+}
