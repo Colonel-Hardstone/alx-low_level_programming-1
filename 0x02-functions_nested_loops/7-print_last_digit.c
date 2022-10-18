ef2571f957a169bacfeb31fc0eba5fb39c8c36d3
MaeregSahilu <maereg.sahilu.a@gmail.com>
2022-10-18 15:36:18 +0000

    chance mr bond

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+   *print_last_digit - returns the last digit of a number
+   *@n: integer input
+   *
+   *Return: This function returns a postive int number
+   */
+
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
