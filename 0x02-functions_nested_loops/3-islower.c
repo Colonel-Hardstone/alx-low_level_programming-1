b41406055b60425df1c151ad9cf4017314ba50fd
MaeregSahilu <maereg.sahilu.a@gmail.com>
2022-10-18 14:39:26 +0000

    if probably rejects

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+   *_islower - This function prints the alphabet using putchar
+   *
+   *Return: This function returns 0 on success
+   */
+
+int _islower(int c)
+{
+
+	if (i <= 122 && i >= 97)
+	{
+		return (1);
+	}
+	else 
+	return (0);
+}
