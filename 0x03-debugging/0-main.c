1eb7a2f81b097c873e0d9899399f0b8a1dfbb302
MaeregSahilu <maereg.sahilu.a@gmail.com>
2022-10-19 09:33:21 +0000

    you are HIV aladeen

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+   *main - This function tests the postive or negative function
+   *
+   *Return: This function returns 0 on success
+   */
+
+int main(void)
+{
+	int i = 0;
+	
+	postive_or_negative(i);
+
+	return (0);
+}
