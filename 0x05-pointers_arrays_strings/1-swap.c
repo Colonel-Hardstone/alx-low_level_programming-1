eaebdb20f1a7e19144651324ca65ba8c71648b01
MaeregSahilu <maereg.sahilu.a@gmail.com>
2022-10-24 14:43:46 +0000

    dot your ts and cross your is

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the value of two integers
+ * 
+ * @a: first integer
+ * @b: second integer
+ * Return: Nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	int *p;
+	
+	p = &a;
+	*p = *b;
+	*b = p;
+}
