367c69fee6e622be37fb5d1906ea0a1a61f69297
MaeregSahilu <maereg.sahilu.a@gmail.com>
2022-11-14 22:32:36 +0000

    mosdef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+  *struct dog - Doggo.
+  *@name: Name of doggo.
+  *@age: How old is doggo.
+  *@owner: Who's good boy is the doggo.
+  *
+  *Description: woof woof.
+  */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
