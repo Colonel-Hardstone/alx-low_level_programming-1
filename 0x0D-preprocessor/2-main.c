a9d5763ca21f44aa09a03a1f33472c8e30d8639d
MaeregSahilu <maereg.sahilu.a@gmail.com>
2022-11-14 17:23:30 +0000

    preproc

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
   *main - A program prints name of the file compiled from and a new line.
@@ -6,5 +7,16 @@
 
 void main(void)
 {
-	printf("%s\n", __FILE__);
+	char file[] = __FILE__;
+
+	for (int count = 0; count < 80; count++)
+	{
+		if (file[count] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		else
+			_putchar(file[count]);
+	}
 }
